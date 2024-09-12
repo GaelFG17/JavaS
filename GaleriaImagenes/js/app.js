5ca97e2fdf3000501ee19a2244750f4afb0038ee
GaelFG17 <gaelfg1720@gmail.com>
2024-09-11 18:13:01 -0600

    Seleccionar los elementos del html

---
+++ GaleriaImagenes/js/app.js
@@ -0,0 +1,4 @@
+const modal = document.querySelector('.modal');
+const modalImg = document.getElementById('imgGrande');
+const captionText = document.getElementById('caption');
+const closeModal = document.querySelector('.close');
