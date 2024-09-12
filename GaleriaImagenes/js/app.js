96637ddfa8e4df7d39501f98ae0896f868f493b4
GaelFG17 <gaelfg1720@gmail.com>
2024-09-11 18:15:16 -0600

    Bloque de codigo con la funcion para abrir el modal de la imagen

--- GaleriaImagenes/js/app.js
+++ GaleriaImagenes/js/app.js
@@ -2,3 +2,12 @@ const modal = document.querySelector('.modal');
 const modalImg = document.getElementById('imgGrande');
 const captionText = document.getElementById('caption');
 const closeModal = document.querySelector('.close');
+
+document.querySelectorAll('.gallery img').forEach(img => {
+    img.addEventListener('click', () => {
+        modal.style.display = 'block';
+        modalImg.src = img.src;
+        captionText.innerHTML = img.alt;
+    });
+});
+
