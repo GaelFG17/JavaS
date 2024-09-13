521d272f736f90f2bf9dcb28c1d6af06feb884bc
GaelFG17 <gaelfg1720@gmail.com>
2024-09-12 23:04:55 -0600

    Creacion de la contraseña con un ciclo for

--- contras/app.js
+++ contras/app.js
@@ -13,4 +13,11 @@ function generatePassword() {
     if (includeUppercase) characters += uppercase;
     if (includeNumbers) characters += numbers;
     if (includeSymbols) characters += symbols;
+
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        password += characters[Math.floor(Math.random() * characters.length)];
+    }
+    
+    document.getElementById('result').textContent = password;
 }
