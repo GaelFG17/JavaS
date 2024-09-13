637bf255e81dd2820918294274b3cbf8dcf757f5
GaelFG17 <gaelfg1720@gmail.com>
2024-09-12 21:02:11 -0600

    Inicio de la Funcion para generar la contraseña obteniedo los elementos del html

---
+++ contras/app.js
@@ -0,0 +1,6 @@
+function generatePassword() {
+    const length = document.getElementById('length').value;
+    const includeUppercase = document.getElementById('uppercase').checked;
+    const includeNumbers = document.getElementById('numbers').checked;
+    const includeSymbols = document.getElementById('symbols').checked;
+}
