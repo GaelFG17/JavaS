db8fbb7eaac201429e914c989eab68be7c28ec19
GaelFG17 <gaelfg1720@gmail.com>
2024-09-13 19:56:43 -0600

    Creacion de alerta y variable con el url de la peticion

--- jsapi/app.js
+++ jsapi/app.js
@@ -1,4 +1,11 @@
 document.getElementById('getWeather').addEventListener('click', function() {
     const city = document.getElementById('city').value;
     const apiKey = '';
+
+    if (city === "") {
+        alert("Por favor, ingresa el nombre de una ciudad.");
+        return;
+    }
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 });
